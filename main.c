8b51ec3c0e471cb3146775e1e69690a1b87fbf14
NnamOzioma <nnamdorathy23@gmail.com>
2023-04-28 15:01:56 -0700

    all

---
+++ main.c
@@ -0,0 +1,131 @@
+#include "main.h"
+
+/**
+* sig_handler - Prints a new prompt upon a signal.
+* @sig: The signal.
+*/
+void sig_handler(int sig)
+{
+	char *new_prompt = "\n#cisfun$ ";
+
+	(void)sig;
+	signal(SIGINT, sig_handler);
+	write(STDIN_FILENO, new_prompt, 10);
+}
+
+/**
+* @args: An array of arguments.
+* exec - Execs a command in a child process.
+* @front: A double pointer to the beginning of args.
+*
+* Return: If an error occurs - a corresponding error code.
+*/
+int exec(char **args, char **front)
+{
+	pid_t child_pid;
+	int status, flag = 0, ret = 0;
+	char *command = args[0];
+	
+	if (command[0] != '/' && command[0] != '.')
+	{
+		flag = 1;
+		command = get_loc(command);
+	}
+
+	if (!command || (access(command, F_OK) == -1))
+	{
+		if (errno == EACCES)
+			ret = (error_create(args, 126));
+		else
+			ret = (error_create(args, 127));
+	}
+	else
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			if (flag)
+				free(command);
+			perror("Error child:");
+			return (1);
+		}
+		if (child_pid == 0)
+		{
+			execve(command, args, environ);
+			if (errno == EACCES)
+				ret = (error_create(args, 126));
+			free_env();
+			free_args(args, front);
+			free_alias_list(aliases);
+			_exit(ret);
+		}
+		else
+		{
+			wait(&status);
+			ret = WEXITSTATUS(status);
+		}
+	}
+	if (flag)
+		free(command);
+
+	return (ret);
+}
+
+/**
+* main - Runs a simple UNIX command interpreter.
+* @argc: The number of arguments given by user
+* @argv: An array of pointers to the arguments.
+*
+* Return: The return value of the last execd command.
+*/
+int main(int argc, char *argv[])
+{
+	int ret = 0, retn;
+	int *exe_ret = &retn;
+	char *prompt = "#cisfun$ ", *new_line = "\n";
+
+	name = argv[0];
+	hist = 1;
+	aliases = NULL;
+	signal(SIGINT, sig_handler);
+
+	*exe_ret = 0;
+	environ = _cpenv();
+	if (!environ)
+		exit(-100);
+
+	if (argc != 1)
+	{
+		ret = proc_file_commands(argv[1], exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
+
+	if (!isatty(STDIN_FILENO))
+	{
+		while (ret != END_OF_FILE && ret != EXIT)
+			ret = handle_args(exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
+
+	while (1)
+	{
+		write(STDOUT_FILENO, prompt, 10);
+		ret = handle_args(exe_ret);
+		if (ret == END_OF_FILE || ret == EXIT)
+		{
+			if (ret == END_OF_FILE)
+				write(STDOUT_FILENO, new_line, 10);
+			free_env();
+			free_alias_list(aliases);
+			exit(*exe_ret);
+		}
+	}
+
+	free_env();
+	free_alias_list(aliases);
+	return (*exe_ret);
+}
